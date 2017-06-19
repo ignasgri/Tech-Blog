72c729c7de4eca5bc93b0bd4cbf3f9ceb32f2a82
Ignas Grigaravicius <kickeris@gmail.com>
2017-06-19 18:19:10 +0100

    working, but not finished

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'django.contrib.sites',  
+    'disqus',
+    'django_forms_bootstrap',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS= (
+    os.path.join(BASE_DIR, "static"),
+)
+
+DISQUS_WEBSITE_SHORTNAME = 'techmonster'
+SITE_ID = 1
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
