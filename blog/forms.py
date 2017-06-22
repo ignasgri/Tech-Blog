fe21954168bc190837371070e184c587f740a831
Ignas Grigaravicius <kickeris@gmail.com>
2017-06-22 09:09:12 +0100

    reset password update

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content','image')
+        fields = ('title', 'content','tag', 'image')
