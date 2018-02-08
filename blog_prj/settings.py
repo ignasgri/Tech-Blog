c730b2eab48e7298e0ace6d76c69e5677b55278f
Ignas Grigaravicius <kickeris@gmail.com>
2018-02-08 08:23:00 +0000

    uploading to heroku

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+# import os
 import dj_database_url
 from .env import *
 
@@ -93,10 +93,12 @@ WSGI_APPLICATION = 'blog_prj.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASE_URL = 'postgres://dzbmpemarbnftb:2049a6e14b0d78c25e7f0892dd1970f1707c9cc6adc1aa130010c710e29e7606@ec2-54-217-212-8.eu-west-1.compute.amazonaws.com:5432/dbcrj3eks40gh0'
-DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+
+# DATABASE_URL = os.environ.get('DATABASE_URL')
+# DATABASE_URL = 'postgres://dzbmpemarbnftb:2049a6e14b0d78c25e7f0892dd1970f1707c9cc6adc1aa130010c710e29e7606@ec2-54-217-212-8.eu-west-1.compute.amazonaws.com:5432/dbcrj3eks40gh0'
+# DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
 # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
