b40bdf9ae547a3dd7fbd8439da62dca0cef25572
Ignas Grigaravicius <kickeris@gmail.com>
2018-02-08 08:07:19 +0000

    changes to improve page visual appeal

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -93,9 +93,10 @@ WSGI_APPLICATION = 'blog_prj.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-# DATABASE_URL = os.environ.get('DATABASE_URL')
-# DATABASE_URL = 'postgres://dzbmpemarbnftb:2049a6e14b0d78c25e7f0892dd1970f1707c9cc6adc1aa130010c710e29e7606@ec2-54-217-212-8.eu-west-1.compute.amazonaws.com:5432/dbcrj3eks40gh0'
+DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = 'postgres://dzbmpemarbnftb:2049a6e14b0d78c25e7f0892dd1970f1707c9cc6adc1aa130010c710e29e7606@ec2-54-217-212-8.eu-west-1.compute.amazonaws.com:5432/dbcrj3eks40gh0'
 DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
